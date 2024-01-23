8cce47c7d5d53e7d31f3e40029f6ecd3229d5387
theboyGolden <preciousstick15@gmail.com>
2024-01-23 18:55:48 +0000

    exception handling for division by zero added

--- interpreter.py
+++ interpreter.py
@@ -10,5 +10,10 @@ elif y == "*":
     print (int(x) * int(z))
 elif y == "/":
     print (int(x) / int(y))
+    # We now check if division by zero is attempted
+    if z == 0:
+        raise ValueError("Division by zero is not allowed.")
+    result = x / z
 else:
     print ("Enter a valid arithmetic expression")
+    

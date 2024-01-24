9761ad27a404df4beaf448b2063a7ee35d1a2dae
theboyGolden <preciousstick15@gmail.com>
2024-01-24 17:12:00 +0000

    .

--- seasons.py
+++ seasons.py
@@ -2,7 +2,10 @@ from datetime import date
 
 
 def main():
-    ...
+    DOB = input ("Input your date of birth in the format 1999-01-27")
+    today = date.today()
+    age_years = today.year - int(DOB[:4])
+    
 
 
 ...
